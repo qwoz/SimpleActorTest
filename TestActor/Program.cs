5315b5f33ce6454328ecd07993da0d0ae63d3cbb
qwoz <qwoz@users.noreply.github.com>
2016-06-19 12:29:01 -0700

    Example solution

---
+++ TestActor/Program.cs
@@ -0,0 +1,20 @@
+﻿using Akka.Actor;
+using Akka.Routing;
+using System;
+
+namespace TestActor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ActorSystem sys = ActorSystem.Create("MySystem");
+
+            IActorRef actor = sys.ActorOf(Props.Create(() => new MyActor()).WithRouter(FromConfig.Instance), "pool");
+            var reply = actor.Ask<string>("ping").Result;
+
+            Console.Out.WriteLine("Success: " + reply);
+            Console.In.ReadLine();
+        }
+    }
+}
---
+++ TestActor/Tester.cs
@@ -0,0 +1,28 @@
+﻿using Akka.Actor;
+using Akka.Routing;
+using Akka.TestKit.NUnit;
+using NUnit.Framework;
+
+namespace TestActor
+{
+    [TestFixture]
+    public class Tester : TestKit
+    {
+        [Test]
+        public void CreatePoolFromConfig()
+        {
+            IActorRef actor = Sys.ActorOf(Props.Create<MyActor>().WithRouter(FromConfig.Instance), "pool");
+        }
+    }
+
+    public class MyActor : ReceiveActor
+    {
+        public MyActor()
+        {
+            ReceiveAny(ping =>
+            {
+                Sender.Tell("pong");
+            });
+        }
+    }
+}
